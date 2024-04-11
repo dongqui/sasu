cb37494713b3535fa0f3869bdc7bb9c3b3c66a7d
Dongjin <dongqui9@gmail.com>
2024-04-11 16:11:49 +0900

    feat: 관리자 페이지 수집 막기

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
+
 import { AdminHeader } from "@/widgets/header/ui/AdminHeader";
 import { AddLinkForm } from "@/features/admin/ui/AddLinkForm";
 import { EditLinkList } from "@/features/admin/ui/EditLinkList";
+
+export const metadata: Metadata = {
+  robots: "noindex, nofollow",
+};
 
 export default function Admin() {
   return (
